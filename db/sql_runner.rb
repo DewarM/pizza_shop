6f9031b9d6dd73621e562fe35c90bfdafe211f3e
Mungo Dewar <mpjdewar@gmail.com>
2017-05-03 09:57:41 +0100

    Add env config files for heroku local and configure RACK_ENV dev and prod database conn

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -2,8 +2,13 @@ require('pg')
 class SqlRunner
 
   def self.run( sql )
+
     begin
-      db = PG.connect(ENV['DATABASE_URL'])
+      if ENV['RACK_ENV'] == 'development'
+        db = PG.connect({dbname: 'pizza_shop', host: 'localhost'})
+      else
+        db = PG.connect(ENV['DATABASE_URL'])
+      end
       result = db.exec( sql )
     ensure
       db.close
