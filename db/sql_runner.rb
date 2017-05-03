4519f0e23a31cc023906dda1d32734a620b6f6d2
Mungo Dewar <mpjdewar@gmail.com>
2017-05-03 08:54:45 +0100

    Db name change

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -3,7 +3,7 @@ class SqlRunner
 
   def self.run( sql )
     begin
-      db = PG.connect({ dbname: 'pizza_shop', host: ENV['DATABASE_URL'] || 'localhost' })
+      db = PG.connect({ dbname: 'dlcaih0192iou', host: ENV['DATABASE_URL'] || 'localhost' })
       result = db.exec( sql )
     ensure
       db.close
